2b4046590243377d3bc13e815898d1d07628a7f2
Abhishek Kumar <93374250+apintu705@users.noreply.github.com>
2022-05-29 14:45:18 +0530

    home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,24 @@
+import axios from "axios";
 import React from "react";
-import "./home.scss"
+import "./home.scss";
+import Movielisting from "../movielisting/movielisting";
 
 const Home = () => {
-  return();
+  React.useEffect(() => {
+    const fetchdata = async () => {
+      const {
+        data
+      } = await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=7a5e99c0
+      &s="Harry"`);
+      console.log(data);
+    };
+    fetchdata();
+  }, []);
+  return (
+    <div>
+      <div className="bannerimage"></div>
+      <Movielisting />
+    </div>
+  );
 };
 export default Home;
