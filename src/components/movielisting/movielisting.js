8d85c7a6fa878957974e736f5efd2456a723956f
Abhishek Kumar <93374250+apintu705@users.noreply.github.com>
2022-05-30 16:12:38 +0530

    egr

--- src/components/movielisting/movielisting.js
+++ src/components/movielisting/movielisting.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {settings} from "../../common/setting.js"
+import Slider from "react-slick";
 import { useSelector } from "react-redux";
 import "./movielisting.scss";
 import Moviecard from "../moviecard/moviecard";
 
+
+
+
 const Movielisting = () => {
   const { movies, loading } = useSelector((state) => state.movies);
   const { shows } = useSelector((state) => state.shows);
+
+  
 
   return (
     <div className="">
@@ -15,12 +22,15 @@ const Movielisting = () => {
         <>
           <div className="movielist">
             <h2>Movies</h2>
+            
             <div className="cardcontainer">
+              
               {movies.Search &&
                 movies.Search.map((movie, index) => (
                   <Moviecard key={index} movie={movie} />
                 ))}
             </div>
+            
           </div>
 
           <div className="movielist">
