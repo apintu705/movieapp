e362e558e445171518fe8a80c4bea5a5c3eca5b5
Abhishek Kumar <93374250+apintu705@users.noreply.github.com>
2022-05-30 15:07:22 +0530

    det

--- src/components/moviedetails/moviedetails.js
+++ src/components/moviedetails/moviedetails.js
@@ -12,6 +12,26 @@ const Moviedetails = () => {
   React.useEffect(() => {
     dispatch(detailsactionfunc(id));
   }, [dispatch, id]);
-  return <div>moviedetails</div>;
+  return (
+    <div className="moviesection">
+      <div className="sectionleft">
+        <div className="movietitle">{details.Title} </div>
+        <div className="movierating">
+          <span>
+            IMDB Rating<i className="fa fa-star"></i> : {details.imdbRating}
+          </span>
+          <span>
+            IMDB Votes:<i className="fa fa-thumbs-up"></i> : {details.imdbVotes}
+          </span>
+          <span>
+            IMDB Votes:<i className="fa fa-film"></i> : {details.Runtime}
+          </span>
+          <span>
+            IMDB Votes:<i className="fa fa-calendar"></i> : {details.Year}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Moviedetails;
