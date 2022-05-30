40ea6c04f0c7c04f2de9a2d394abddbcbd02ccda
Abhishek Kumar <93374250+apintu705@users.noreply.github.com>
2022-05-30 11:38:54 +0530

    store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./features/store";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -9,7 +11,9 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
